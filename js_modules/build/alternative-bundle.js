f9faeed5c037569816e6b52e9403826c5f1ed7d3
Lucas Floriani <lucas.floriane@gmail.com>
2018-06-14 21:31:59 -0300

    First webpack mini project with alternative bundle example

---
+++ js_modules/build/alternative-bundle.js
@@ -0,0 +1,14 @@
+var myModules = [
+  function() {
+    const sum = (a, b) => a + b;
+    return sum;
+  },
+  function() {
+    const sum = myModules[0]();
+    const total = sum(10, 10);
+    console.log(total);
+  }
+];
+
+var entryPointIndex = 1;
+myModules[entryPointIndex]();
